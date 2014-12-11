79fb84bcf8aef6a75679cab3446b10268a568156
things23 <twothreethings@gmail.com>
2014-12-11 15:28:04 +0300

    Add questions controller with tests

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,32 @@
 class QuestionsController < ApplicationController
-  def show
-    @question = Question.find(params[:id])
+  before_action :load_question, only: :show
+  def index
+    @questions = Question.all
   end
+
+  def show
+  end
+
   def new
     @question = Question.new
+  end
+
+  def create
+    @question = Question.new(questions_params)
+    if @question.save
+      redirect_to @question
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:id])
+  end
+
+  def questions_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,6 +2,11 @@ FactoryGirl.define do
   factory :question do
     title "MyString"
     body "MyText"
+
+    factory :invalid_question, class: "Question" do
+      title nil
+      body nil
+    end
   end
 
 end
