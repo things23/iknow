c1c57e28e6a11206c042ce937a2de1c304192f5c
things23 <twothreethings@gmail.com>
2015-01-24 12:41:16 +0300

    fix acceptance test delete answer. not work

--- spec/acceptance/answers/delete_answer_spec.rb
+++ spec/acceptance/answers/delete_answer_spec.rb
@@ -8,15 +8,13 @@ feature "Delete answer", %q{
   given(:user) { create(:user) }
   given(:question) { create(:question, user_id: user.id) }
   given!(:answer) { create(:answer, user_id: user.id, question_id: question.id) }
-  #чsomething wrong!
   scenario "Authenticated user delete question", js: true do
     sign_in(user)
 
     visit question_path(question)
-    within ".answer" do
+    within ".answers" do
       click_on "Delete"
+      expect(page).to_not have_content "MyText"
     end
-    # проверить отсутствие удаленнгго ответа
-    expect(page).to_not have_content "MyText"
   end
 end
