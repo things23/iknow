e617677fea96c9d6cfc83b3bbfeaeb51681c668e
things23 <twothreethings@gmail.com>
2015-05-26 21:09:22 +0300

    Remove comments

--- app/models/new_answer_worker.rb
+++ app/models/new_answer_worker.rb
@@ -3,15 +3,12 @@ class NewAnswerWorker
 
 
   def perform(answer_id)
-    #question = Question.find(question_id)
-    #answer = question.answers.find(answer_id)
     answer = Answer.find(answer_id)
     question = answer.question
     user = question.user
     subscribers = question.subscribers
     subscribers << user
 
-    #user.send_notification_about_answer(answer)
     subscribers.each do |subscriber|
       subscriber.send_notification_about_answer(answer)
     end
